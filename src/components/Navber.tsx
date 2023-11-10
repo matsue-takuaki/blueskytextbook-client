2e54bdb6cdc0f6b3f604327b59ce273b7014c4b2
takuaki <matsutakuaki@gmail.com>
2023-11-11 02:00:52 +0900

    done

--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { auth } from "@/lib/firebase";
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
 
 function Navber(school: any) {
   const currentUniversity = school.school.school_name;
+  const photoUrl:string | StaticImport = auth.currentUser?.photoURL as string;
   return (
-    <div className="w-5/6 mx-auto">
-      <h1 className="py-8 text-teal-300 text-5xl font-bold">
-        青空教科書　ー{currentUniversity}ー
+    <div className="w-5/6 mx-autofflex">
+      <h1 className="py-8 text-teal-300 text-5xl font-bold block">
+        青空教科書　ー {currentUniversity} ー
       </h1>
+      {/* <Image src={photoUrl} width={500} height={500} alt="教科書の写真"/> */}
+      <img src={photoUrl} alt="プロフィール写真" className="block" />
     </div>
   );
 }
