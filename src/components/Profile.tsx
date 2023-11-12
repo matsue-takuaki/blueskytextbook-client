22f645984d00629caa9699943b3f3542d57914b2
takuaki <matsutakuaki@gmail.com>
2023-11-13 07:22:05 +0900

    プロフィール六割

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/lib/firebase";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import React from "react";
+
+function Profile(school:any) {
+  const photoUrl: string | StaticImport = auth.currentUser?.photoURL as string;
+  return (
+    <div>
+      <div className="flex items-center justify-evenly">
+        <img src={photoUrl} className="rounded-full " alt="プロフィール写真" />
+        <p className="text-3xl">
+          名前： {auth.currentUser?.displayName}
+        </p>
+        <p className="text-3xl">学校： {school.school}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import NavbarProfile from "@/components/NavbarProfile";
+import Profile from "@/components/Profile";
+import { auth } from "@/lib/firebase";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { apiClient, apiSchool } from "@/lib/apiClient";
+import Link from "next/link";
 
-function profile() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  return {
+    props: {
+      id,
+    },
+  };
+};
+
+function profile({ id }: any) {
+  const router = useRouter();
+  const [user] = useAuthState(auth);
+  const [school, setSchool] = useState<string>("");
+  const [schoolData,setSchoolData] = useState("");
+  // useEffect(() => {
+  //   if (!user) {
+  //     router.push("/");
+  //   }
+  // }, [user]);
+  useEffect(() => {
+    // if(auth.currentUser?.uid != id){
+      //     alert("あなたはこのページに閲覧することはできません");
+      //     router.push("/");
+      //   }
+    const getProfile = async () => {
+      const email = auth.currentUser?.email;
+      const user = await apiClient.post("/auth/get", {
+        email,
+      });
+      await apiSchool.get(`https://api.edu-data.jp/api/v1/school?keyword=${user.data.user.school}`).then((response) =>{
+        setSchoolData(response.data.schools.data[0].school_code);
+      })
+      setSchool(user.data.user.school);
+    };
+    getProfile();
+  }, []);
   return (
-    <div>profile</div>
-  )
+    <div className="h-screen bg-white">
+      <NavbarProfile />
+      <Link href={`/transactions/${schoolData}`}>
+        <div className="flex">
+          <ArrowBackIosNewIcon/>
+          <p className="font-bold">戻る</p>
+        </div>
+      </Link>
+      <div className="w-3/4 mx-auto mt-4">
+        <Profile school={school}/>
+      </div>
+    </div>
+  );
 }
 
-export default profile
+export default profile;
